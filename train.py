c0b37df1c1221cf9a7034f11a2f0a84d124b3c81
Batuhan <batuhan.undar01@gmail.com>
2018-12-19 23:04:37 +0300

    - change splitted plot to subplots - add batch loss - add improved model saving

--- train.py
+++ train.py
@@ -7,8 +7,9 @@ import argparse
 
 
 DEFAULT_DUMP_FILE = 'trained_model.pkl'  # default model file name
-SHOW_PLOT = True  # show graph of chane in accuracy and loss over epochs
-VALIDATE_DATA = True
+SHOW_PLOT = True  # show graph of change in accuracy and loss over epochs
+VALIDATE_DATA = True  # use a validation on training (requires validation path from cli)
+DUMP_BEST_MODEL = True  # only dump model if accuracy improved
 
 
 def parameter_parse(argv):
@@ -19,8 +20,8 @@ def parameter_parse(argv):
     """
     parser = argparse.ArgumentParser('Train a neural network')
     parser.add_argument('--data_path', '-d', required=True,
-                        dest='data_path', type=str, nargs=1, help='Path to the training data')
-    parser.add_argument('--model_name', '-m', type=str, nargs=1, default=DEFAULT_DUMP_FILE,
+                        dest='data_path', type=str, help='Path to the training data')
+    parser.add_argument('--model_name', '-m', type=str, default=DEFAULT_DUMP_FILE,
                         dest='model_name', help='name for model, if not given default name will be used')
     parser.add_argument('--template_path', '-t', required=False, type=str, default='model.json',
                         dest='template_path', help='template from file, if not given default path will be used')
@@ -38,7 +39,7 @@ if __name__ == '__main__':
     args = parameter_parse(sys.argv[1:])
 
     # prepare training data
-    train = load_data(args['data_path'][0])
+    train = load_data(args['data_path'])
     train_x = [normalize(x, 255) for x in train['x']]
     train_y = output2binary(train['y'][0])
 
@@ -61,42 +62,66 @@ if __name__ == '__main__':
 
     for epoch in range(NN.epochs):
 
-        NN.dropout_probability = 0.0
+        NN.dropout_probability = 0.5
         NN.fit(train_x, train_y)
 
         # get results
         print("Current epoch: %d" % epoch)
         print("Learning rate: %f" % NN.l_rate)
-        print("Loss: %f" % (NN.out_layer.cost / len(train_x)))
+        print("Loss: %f" % NN.batch_loss)
         print("Accuracy: %f" % ((NN.hit_count / len(train_x)) * 100))
-        print("Last Prediction: {}".format(NN.out_layer.predicted))
-        print("Expected: {}".format(train_y[-1]))
         print("Hit count: %d" % NN.hit_count)
 
         # accumulate the graph data
-        losses.append(NN.out_layer.cost / len(train_x))
+        losses.append(NN.batch_loss)
         accuracies.append((NN.hit_count / len(train_x)))
 
         if VALIDATE_DATA:
+            # Use validation data
             NN.dropout_probability = 0.0
             validation_accuracy = (NN.validate(validate) / len(validate['x']))
             print("Validation accuracy: {}".format(validation_accuracy * 100))
             validation_accuracies.append(validation_accuracy)
 
-        print("-"*20+"\n")
+            if max(validation_accuracies) <= validation_accuracy and DUMP_BEST_MODEL:
+                # dump model if accuracy improved
+                print("Model dumped")
+                NN.dump(args['model_name'])
+        else:
+            # Validation data is not available, use training accuracy instead
 
+            if max(accuracies) <= ((NN.hit_count / len(train_x)) * 100) and DEFAULT_DUMP_FILE:
+                # save model to file if accuracy of it improved
+                print("Model dumped")
+                NN.dump(args['model_name'])
+
+        if not DUMP_BEST_MODEL:
+            # dump model to file at every epoch
+            NN.dump(args['model_name'])
+
+        # clean model for new epoch
         NN.clean()
 
-        # save model to file
-        NN.dump(args['model_name'][0])
+        print("-"*20+"\n")
 
     if SHOW_PLOT:
         # graph accuracies and loss
+        fig, axs = plt.subplots(1, 2)
 
         if VALIDATE_DATA:
-            plt.plot([i for i in range(NN.epochs)], validation_accuracies, label='Validation accuracy')
+            # also plot validation over epochs if available
+            axs[0].plot([i for i in range(NN.epochs)], validation_accuracies, label='Validation accuracy')
 
-        plt.plot([i for i in range(NN.epochs)], losses, label='Loss')
-        plt.plot([i for i in range(NN.epochs)], accuracies, label='Accuracy')
-        plt.legend()
+        axs[0].plot([i for i in range(NN.epochs)], accuracies, label='Training Accuracy')
+        axs[1].plot([i for i in range(NN.epochs)], losses, label='Loss')
+
+        axs[0].set_xlabel("Epochs")
+        axs[1].set_xlabel("Epochs")
+
+        axs[0].set_title("Accuracy changes over epochs")
+        axs[1].set_title("Loss change over epochs")
+
+        axs[0].legend()
+        axs[1].legend()
+
         plt.show()
